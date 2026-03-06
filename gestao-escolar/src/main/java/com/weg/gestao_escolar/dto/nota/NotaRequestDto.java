b811e118009624c68e6b2c12800244bbe343fffa
VINICIUS DOS SANTOS ZAPELLA <vinicius_zapella@estudante.sesisenai.org.br>
2026-03-06 20:07:19 -0300

    feat: feito a verificação do dto da noa

--- gestao-escolar/src/main/java/com/weg/gestao_escolar/dto/nota/NotaRequestDto.java
+++ gestao-escolar/src/main/java/com/weg/gestao_escolar/dto/nota/NotaRequestDto.java
@@ -1,8 +1,17 @@
 package com.weg.gestao_escolar.dto.nota;
 
+import jakarta.validation.constraints.*;
+
 public record NotaRequestDto(
+        @NotNull(message = "o id nao poder ser nulo")
+        @Min(value = 1, message = "O id deve ser valido")
         int alunoId,
+        @NotNull(message = "o id nao poder ser nulo")
+        @Min(value = 1, message = "O id deve ser valido")
         int aulaId,
+        @NotNull(message = "O valor nao pode ser nulo")
+        @PositiveOrZero(message = "O valor nao pode ser negativa")
+        @Max(value = 10, message = "A nota máxima permitida é 10")
         Double valor
 ) {
 }

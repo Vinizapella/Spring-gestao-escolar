5b594d9507998939e72cff7134dd94dbd8fe168a
VINICIUS DOS SANTOS ZAPELLA <vinicius_zapella@estudante.sesisenai.org.br>
2026-03-06 20:01:02 -0300

    feat: feito a verificação do dto da aula

--- gestao-escolar/src/main/java/com/weg/gestao_escolar/dto/aula/AulaRequestDto.java
+++ gestao-escolar/src/main/java/com/weg/gestao_escolar/dto/aula/AulaRequestDto.java
@@ -1,10 +1,19 @@
 package com.weg.gestao_escolar.dto.aula;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+
 import java.time.LocalDateTime;
 
 public record AulaRequestDto(
+        @NotNull(message = "Obrigatorio a turma")
+        @Min(value = 1, message = "Id deve ser valido")
         int turmaId,
+        @NotNull(message = "a hora nao pode ser nula")
         LocalDateTime dataHora,
+        @NotBlank(message = "Assunto nao pode ser em branco")
         String assunto
 ) {
 }

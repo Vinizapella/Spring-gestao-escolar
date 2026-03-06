ff4470393849a3a715084d2a980fa6b9f2f44d0c
VINICIUS DOS SANTOS ZAPELLA <vinicius_zapella@estudante.sesisenai.org.br>
2026-03-06 19:55:57 -0300

    feat: feito a verificação do dto do aluno

--- gestao-escolar/src/main/java/com/weg/gestao_escolar/dto/aluno/AlunoRequestDto.java
+++ gestao-escolar/src/main/java/com/weg/gestao_escolar/dto/aluno/AlunoRequestDto.java
@@ -1,11 +1,20 @@
 package com.weg.gestao_escolar.dto.aluno;
 
+import jakarta.validation.constraints.*;
+
 import java.time.LocalDate;
 
 public record AlunoRequestDto(
+        @NotBlank(message = "Nome é Obrigatorio")
+        @Size(min = 3, max = 50, message = "o minimo de caracteres é de 3 e o maximo é 50")
         String nome,
+        @NotBlank(message = "O email e obrigatorio")
+        @Email(message = "Email invalido")
         String email,
+        @NotBlank(message = "Matricula nao pode estar em branco")
         String matricula,
+        @NotNull(message = "Obrigatorio ter data de nascimento")
+        @Past(message = "A data de nascimento deve ser de uma data passada")
         LocalDate data_nascimento
 ) {
 }

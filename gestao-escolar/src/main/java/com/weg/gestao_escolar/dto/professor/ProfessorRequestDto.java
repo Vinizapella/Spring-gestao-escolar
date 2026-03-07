daba9a329199de42af5fe83fdfbe034498a30f8a
VINICIUS DOS SANTOS ZAPELLA <vinicius_zapella@estudante.sesisenai.org.br>
2026-03-06 21:03:18 -0300

    feat: professor feito

--- gestao-escolar/src/main/java/com/weg/gestao_escolar/dto/professor/ProfessorRequestDto.java
+++ gestao-escolar/src/main/java/com/weg/gestao_escolar/dto/professor/ProfessorRequestDto.java
@@ -1,8 +1,18 @@
 package com.weg.gestao_escolar.dto.professor;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record ProfessorRequestDto(
+        @NotBlank(message = "Nome nao pode ser invalido")
+        @Size(min = 3, max = 50, message = "O nome deve ter no minimo ")
         String nome,
+        @NotBlank(message = "Email é obrigatorio")
+        @Email(message = "Email invalido")
         String email,
+        @NotBlank(message = "Nome nao pode ser invalido")
+        @Size(min = 5, max = 100, message = "O nome deve ter no minimo ")
         String disciplina
 ) {
 }

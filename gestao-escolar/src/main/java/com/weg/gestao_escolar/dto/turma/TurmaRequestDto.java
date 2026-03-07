60093651d2d7363a7e3f8548a81a3b87363f7b65
VINICIUS DOS SANTOS ZAPELLA <vinicius_zapella@estudante.sesisenai.org.br>
2026-03-06 21:05:49 -0300

    feat: finalizado

--- gestao-escolar/src/main/java/com/weg/gestao_escolar/dto/turma/TurmaRequestDto.java
+++ gestao-escolar/src/main/java/com/weg/gestao_escolar/dto/turma/TurmaRequestDto.java
@@ -1,11 +1,21 @@
 package com.weg.gestao_escolar.dto.turma;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import java.util.List;
 
 public record TurmaRequestDto(
+        @NotBlank(message = "nome da turma nao pode ser invalido")
+        @Size(min = 1, max = 50, message = "o minimo é 1 o maximo é 50")
         String nome,
+        @NotNull(message = "nao pode ser nula")
         int cursoId,
+        @NotNull(message = "nao pode ser nula")
         int professorId,
+        @NotEmpty(message = "A lista de alunos não pode estar vazia"
         List<Integer> alunoIds
 ) {
 }
